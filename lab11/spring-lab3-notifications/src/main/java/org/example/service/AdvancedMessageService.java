813391edd11dab32c13850f4ac8b8ec0565f2a10
Student <student@example.com>
2026-04-01 14:06:56 +0300

    upd

--- lab11/spring-lab3-notifications/src/main/java/org/example/service/AdvancedMessageService.java
+++ lab11/spring-lab3-notifications/src/main/java/org/example/service/AdvancedMessageService.java
@@ -1,6 +1,5 @@
 package org.example.service;
 
-// Дополнительное задание Части 4: расширенный интерфейс
 public interface AdvancedMessageService extends MessageService {
     String getServiceType();
 }
